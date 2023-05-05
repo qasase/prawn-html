c1da25d4575a6610eaf1797bc3b6ecfb61d111da
mattiasgullstrand <39914076+mattiasgullstrand@users.noreply.github.com>
2023-05-05 15:54:12 +0200

    Do not indent list items (#1)

--- lib/prawn_html/tags/li.rb
+++ lib/prawn_html/tags/li.rb
@@ -5,9 +5,6 @@ module PrawnHtml
     class Li < Tag
       ELEMENTS = [:li].freeze
 
-      INDENT_OL = -12
-      INDENT_UL = -6
-
       def block?
         true
       end
@@ -18,19 +15,11 @@ module PrawnHtml
         @before_content_once = @counter ? "#{@counter}. " : "#{@symbol} "
       end
 
-      def block_styles
-        super.tap do |bs|
-          bs[:indent_paragraphs] = @indent
-        end
-      end
-
       def on_context_add(_context)
         case parent.class.to_s
         when 'PrawnHtml::Tags::Ol'
-          @indent = INDENT_OL
           @counter = (parent.counter += 1)
         when 'PrawnHtml::Tags::Ul'
-          @indent = INDENT_UL
           @symbol = parent.styles[:list_style_type] || '&bullet;'
         end
       end
